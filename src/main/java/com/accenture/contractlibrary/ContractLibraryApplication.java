96334ab24bae0acdc8f296bf2e14d672a2d5a407
yash6578 <yashchawla6578@gmail.com>
2022-12-12 09:20:46 +0530

    first commit

---
+++ src/main/java/com/accenture/contractlibrary/ContractLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.contractlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/accenture/contractlibrary/ContractLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.contractlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
